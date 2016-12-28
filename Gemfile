1a897422a504da721e39ca82c59fb5189a68ec53
Victor Raft <victoraft@gmail.com  git config --global user.name Victor Raft>
2016-12-28 13:43:48 -0200

    copying files

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'rubyzip'
+gem 'ipa_analyzer'
+
+group :test do
+  gem 'rspec'
+end
---
+++ step.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+set -e
+
+export BUNDLE_GEMFILE="$THIS_SCRIPT_DIR/Gemfile"
+
+echo
+echo "=> Preparing step ..."
+echo
+bundle install --without test --jobs 20 --retry 5
+
+echo
+echo "=> Running the step ..."
+echo
+bundle exec ruby "$THIS_SCRIPT_DIR/step.rb" \
+  -u "${build_url}" \
+  -t "${build_api_token}" \
+  -c "${is_compress}" \
+  -d "${deploy_path}" \
+  -g "${notify_user_groups}" \
+  -e "${notify_email_list}" \
+  -p "${is_enable_public_page}"
