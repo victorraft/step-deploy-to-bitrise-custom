cac8cfa9b90c2f1e777b1696235d46214a5bd4eb
Victor Raft <victoraft@gmail.com>
2017-01-03 18:30:28 -0200

    -

--- uploaders/file_uploader.rb
+++ uploaders/file_uploader.rb
@@ -9,6 +9,20 @@ def deploy_file_to_bitrise(file_path, build_url, api_token)
   puts
   puts "# Deploying file: #{file_path}"
 
+  file_size = File.size(file_path)
+
+  file_info_hsh = {
+      file_size_bytes: file_size,
+      app_info: {
+          app_name: file_path,
+          package_name: 0,
+          version_code: 0,
+          version_name: 0,
+          min_sdk_version: 0
+      }
+  }
+  puts "  (i) file_info_hsh: #{file_info_hsh}"
+
   # - Create a Build Artifact on Bitrise
   puts '--> Create a Build Artifact on Bitrise'
   upload_url, artifact_id = create_artifact(build_url, api_token, file_path, 'file')
@@ -24,7 +38,7 @@ def deploy_file_to_bitrise(file_path, build_url, api_token)
   return finish_artifact(build_url,
                          api_token,
                          artifact_id,
-                         '',
+                         JSON.dump(file_info_hsh),
                          '',
                          '',
                          true
